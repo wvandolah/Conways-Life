0e79de53c2d6ade1eb9bc5a9057f245416b98164
William VanDolah <william.vandolah@gmail.com>
2018-12-11 16:10:10 -0600

    Updates

--- precache-manifest.f02dcfee0392eaaf08e9acae474d03ce.js
+++ precache-manifest.f02dcfee0392eaaf08e9acae474d03ce.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
   },
   {
-    "revision": "35630df99a420c75e21e",
-    "url": "/Conways-Life/static/js/main.35630df9.chunk.js"
+    "revision": "6f71a3ce6614ee793c3e",
+    "url": "/Conways-Life/static/js/main.6f71a3ce.chunk.js"
   },
   {
     "revision": "97168140c21eb8225604",
     "url": "/Conways-Life/static/js/1.97168140.chunk.js"
   },
   {
-    "revision": "35630df99a420c75e21e",
+    "revision": "6f71a3ce6614ee793c3e",
     "url": "/Conways-Life/static/css/main.4cfbc51a.chunk.css"
   },
   {
-    "revision": "3f1b93cb0879c273cea696ddd249dd5f",
+    "revision": "3fd499d46d3fefa811c42cea97523273",
     "url": "/Conways-Life/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Conways-Life/precache-manifest.30ef528811aa62bb23926b3d0bf18342.js"
+  "/Conways-Life/precache-manifest.f02dcfee0392eaaf08e9acae474d03ce.js"
 );
 
 workbox.clientsClaim();
