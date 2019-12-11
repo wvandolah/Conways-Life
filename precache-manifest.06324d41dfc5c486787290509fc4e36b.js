2abc8966fda874750dc66bef37dff8e83d3c2ce6
William VanDolah <william.vandolah@gmail.com>
2019-12-11 09:45:39 -0600

    Updates

---
+++ precache-manifest.06324d41dfc5c486787290509fc4e36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65edad514b081da71990ec4fc1737569",
+    "url": "/Conways-Life/index.html"
+  },
+  {
+    "revision": "7f8a631b4dbbb8e80025",
+    "url": "/Conways-Life/static/css/main.2cc30b5f.chunk.css"
+  },
+  {
+    "revision": "2f0ea64d2606da0a6794",
+    "url": "/Conways-Life/static/js/2.246f9249.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/Conways-Life/static/js/2.246f9249.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f8a631b4dbbb8e80025",
+    "url": "/Conways-Life/static/js/main.ba32799d.chunk.js"
+  },
+  {
+    "revision": "97242d67496ddbea5299",
+    "url": "/Conways-Life/static/js/runtime-main.98838e81.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Conways-Life/precache-manifest.ceb209188f3abcec3e3e3bb6347f5847.js"
+  "/Conways-Life/precache-manifest.06324d41dfc5c486787290509fc4e36b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Conways-Life/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Conways-Life/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
